1a2621b8e3eb11e21eaa2f7c7fdeff0388960a80
Karl Ian Palisoc <krln.plsc@gmail.com>
2025-02-26 23:45:31 +0800

    Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+  "V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+  "v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^",
+  "&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"];
+
+
+function randomPassword(length) {
+  let password = ""
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    password += characters[randomIndex]
+  }
+  return password;
+}
+
+function generatePassword(length) {
+  const password = randomPassword(length)
+ // const encryptedPassword = "*".repeat(password.length)
+  document.getElementById("firstPassword").textContent = password;
+  document.getElementById("secondPassword").textContent = password;
+}
+
+function reset(){
+  let resetter = ""
+  document.getElementById("firstPassword").textContent = resetter
+  document.getElementById("secondPassword").textContent = resetter;
+}
